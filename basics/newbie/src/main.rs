1c8a1d2a7147d0be7a5a20ef05e440c0a3889827
Luciano Callero <lucianocallero@gmail.com>
2024-05-11 20:15:47 +0200

    Going through the Rust book

---
+++ basics/newbie/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    let a = 13;
+    let b =2.3;
+    let c: f32 = 120.0;
+
+
+}
---
+++ basics/variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let guess = "42".parse().expect("Not a number!");
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
